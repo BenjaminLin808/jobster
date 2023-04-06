513254e8ea49de93012461b7d01b8adfbdff328e
BenjaminLin808 <benjaminlin808@gmail.com>
2023-04-05 20:56:42 -0700

    initial commit

---
+++ src/pages/Landing.js
@@ -0,0 +1,33 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta
+            ratione hic ipsa harum at? Voluptatibus nemo impedit accusamus ipsa
+            rem?
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
