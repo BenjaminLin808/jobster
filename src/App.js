5615fc34fbbcf8d30b0d90c8a37f4c158799f959
BenjaminLin808 <benjaminlin808@gmail.com>
2023-04-06 01:18:47 -0700

    finish dashboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing, Error, Dashboard, Register } from "./pages";
+import { Landing, Error, Register, ProtectedRoute } from "./pages";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {
+  AllJobs,
+  Profile,
+  SharedLayout,
+  Stats,
+  AddJob,
+} from "./pages/dashboard";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <SharedLayout />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Stats />} />
+            <Route path="all-jobs" element={<AllJobs />} />
+            <Route path="add-job" element={<AddJob />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
           <Route path="landing" element={<Landing />} />
           <Route path="register" element={<Register />} />
           <Route path="*" element={<Error />} />
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,14 +1,94 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
+import customFetch from "../../utils/axios";
+import {
+  getUserFromLocalStorage,
+  addUserToLocalStorage,
+  removeUserFromLocalStorage,
+} from "../../utils/localStorage";
 
 const initialState = {
   isLoading: false,
-  user: null,
+  isSidebarOpen: false,
+  user: getUserFromLocalStorage(),
 };
+
+export const registerUser = createAsyncThunk(
+  "user/registerUser",
+  async (user, thunkAPI) => {
+    try {
+      const resp = await customFetch.post("/auth/register", user);
+      // console.log(resp);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+      // toast.error(error.response.data.msg);
+      // console.log(error);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "user/loginUser",
+  async (user, thunkAPI) => {
+    try {
+      const resp = await customFetch.post("/auth/login", user);
+      // console.log(resp);
+      return resp.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data.msg);
+      // toast.error(error.response.data.msg);
+      // console.log(error);
+    }
+  }
+);
 
 const userSlice = createSlice({
   name: "user",
   initialState,
+  reducers: {
+    logoutUser: (state) => {
+      state.user = null;
+      state.isSidebarOpen = false;
+      removeUserFromLocalStorage();
+    },
+    toggleSidebar: (state) => {
+      state.isSidebarOpen = !state.isSidebarOpen;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        addUserToLocalStorage(user);
+        toast.success(`Hello There ${user.name}`);
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        toast.error(payload);
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        const { user } = payload;
+        state.isLoading = false;
+        state.user = user;
+        addUserToLocalStorage(user);
+        toast.success(`Welcome Back ${user.name}`);
+      })
+      .addCase(loginUser.rejected, (state, { payload }) => {
+        console.log("here is the problem");
+        state.isLoading = false;
+        toast.error(payload);
+      });
+  },
 });
 
 export default userSlice.reducer;
+export const { toggleSidebar, logoutUser } = userSlice.actions;
