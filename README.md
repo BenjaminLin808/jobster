de730f931cb9502ebf10e1a0ec5168d89f351bed
BenjaminLin808 <benjaminlin808@gmail.com>
2023-04-08 01:18:04 -0700

    finish Stats page

--- README.md
+++ README.md
@@ -2718,7 +2718,7 @@ export const showStats = createAsyncThunk(
   async (_, thunkAPI) => {
     try {
       const resp = await customFetch.get('/jobs/stats');
-      console.log(resp.data));
+      console.log(resp.data);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,14 @@
 import BigSidebar from "./BigSidebar";
+import ChartsContainer from "./ChartsContainer";
 import FormRow from "./FormRow";
 import FormRowSelect from "./FormRowSelect";
 import JobsContainer from "./JobsContainer";
+import Loading from "./Loading";
 import Logo from "./Logo";
 import Navbar from "./Navbar";
 import SearchContainer from "./SearchContainer";
 import SmallSidebar from "./SmallSidebar";
+import StatsContainer from "./StatsContainer";
 
 export {
   Logo,
@@ -16,4 +19,7 @@ export {
   FormRowSelect,
   JobsContainer,
   SearchContainer,
+  Loading,
+  ChartsContainer,
+  StatsContainer,
 };
